797bfe4822f040425cfbf28ceb690143dd66091d
KARTHIK MANIKANDAN RAVIKUMAR <149307601+BEAST-33@users.noreply.github.com>
2025-03-24 22:10:58 +0530

    main file for forward

---
+++ Yash_codes/mainForward.cc
@@ -0,0 +1,25 @@
+#include "ProcessorForward.hpp"
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input_file> <cycle_count>" << std::endl;
+        return 1;
+    }
+    
+    std::string inputFile = argv[1];
+    int cycleCount = std::stoi(argv[2]);
+    
+    ForwardingProcessor processor;
+    
+    if (!processor.loadInstructions(inputFile)) {
+        std::cerr << "Failed to load instructions from file: " << inputFile << std::endl;
+        return 1;
+    }
+    
+    processor.run(cycleCount);
+    processor.printPipelineDiagram();
+    
+    return 0;
+}
